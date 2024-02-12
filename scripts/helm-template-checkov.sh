e3b3b2468cc67ad46c4eed87647b749ec14bf63d
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2024-02-12 11:08:55 +0100

    Ensure all charts renders successfully with checkov values

---
+++ scripts/helm-template-checkov.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+BASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+for chart_file in charts/*/Chart.yaml; do
+    chart_type=$(yq eval '.type' "$chart_file")
+    if [ "$chart_type" = "library" ]; then
+        continue
+    fi
+    cd "$(dirname "$chart_file")"
+    helm template . --values $BASE_DIR/../.checkov-values.yml > /dev/null
+    cd - > /dev/null
+done
+
+echo "All charts can be rendered succesfully."
